6f96bb7ef946a0c0a2777102a80c99e3f696068b
fujiali <1945171729@qq.com>
2021-03-01 13:52:57 +0800

    完成了主页功能的提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,27 @@
 // 注意：每次调用$.get()或$.post()或$.ajax()时，都会先调用ajaxPrefilter这个函数，在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
-    // console.log(options.url);
-    options.url='http://ajax.frontend.itheima.net'+options.url
-    // console.log(options.url);
-  })
+  // 1、在发起真正的Ajax请求之前，统一拼接请求的根路径
+  // console.log(options.url);
+  options.url='http://ajax.frontend.itheima.net'+options.url
+  // console.log(options.url);
+  
+  // 2、统一为有权限的接口，设置headers请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization:localStorage.getItem('token') || ''
+    } 
+  }
+
+  // 3、全局统一挂载complete回调函数
+  options.complete = function (res) {
+    // console.log('执行了回调函数');
+    // console.log(res);
+    // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.message === '身份认证失败! ') {
+        // 1、强制清空token
+        localStorage.removeItem('token')
+        // 2、强制跳转到登录页面
+        location.href='/login.html'
+    }
+ }
+})
